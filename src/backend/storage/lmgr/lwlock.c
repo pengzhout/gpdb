b23b3933fec47bf9b6587f8f7e6154bb1b314f75
Andres Freund <andres@anarazel.de>
2014-12-19 14:29:52 +0100

    Prevent potentially hazardous compiler/cpu reordering during lwlock release.
    In LWLockRelease() (and in 9.4+ LWLockUpdateVar()) we release enqueued
    waiters using PGSemaphoreUnlock(). As there are other sources of such
    unlocks backends only wake up if MyProc->lwWaiting is set to false;
    which is only done in the aforementioned functions.
    
    Before this commit there were dangers because the store to lwWaitLink
    could become visible before the store to lwWaitLink. This could both
    happen due to compiler reordering (on most compilers) and on some
    platforms due to the CPU reordering stores.
    
    The possible consequence of this is that a backend stops waiting
    before lwWaitLink is set to NULL. If that backend then tries to
    acquire another lock and has to wait there the list could become
    corrupted once the lwWaitLink store is finally performed.
    
    Add a write memory barrier to prevent that issue.
    
    Unfortunately the barrier support has been only added in 9.2. Given
    that the issue has not knowingly been observed in praxis it seems
    sufficient to prohibit compiler reordering using volatile for 9.0 and
    9.1. Actual problems due to compiler reordering are more likely
    anyway.
    
    Discussion: 20140210134625.GA15246@awork2.anarazel.de
    
    cherry-pick 37de8de9e33606a043e98fee64b5595aedaa8254

--- src/backend/storage/lmgr/lwlock.c
+++ src/backend/storage/lmgr/lwlock.c
@@ -1210,6 +1210,8 @@ LWLockUpdateVar(LWLock *lock, uint64 *valptr, uint64 val)
 		proc = head;
 		head = proc->lwWaitLink;
 		proc->lwWaitLink = NULL;
+		/* check comment in LWLockRelease() about this barrier */
+		pg_write_barrier();
 		proc->lwWaiting = false;
 		PGSemaphoreUnlock(&proc->sem);
 	}
@@ -1351,6 +1353,16 @@ LWLockRelease(LWLock *lock)
 		proc = head;
 		head = proc->lwWaitLink;
 		proc->lwWaitLink = NULL;
+		/*
+		 * Guarantee that lwWaiting being unset only becomes visible once the
+		 * unlink from the link has completed. Otherwise the target backend
+		 * could be woken up for other reason and enqueue for a new lock - if
+		 * that happens before the list unlink happens, the list would end up
+		 * being corrupted.
+		 *
+		 * The barrier pairs with the SpinLockAcquire() when enqueing for
+		 * another lock.
+		 */
 		pg_write_barrier();
 		proc->lwWaiting = false;
 		PGSemaphoreUnlock(&proc->sem);
