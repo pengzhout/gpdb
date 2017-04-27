c4453bf5652d968da088de84c671e3ccfc560887
Pengzhou Tang <ptang@pivotal.io>
2017-04-26 23:37:49 -0400

    init cgroup properly in resgroup testcases.

--- src/test/isolation2/sql/resgroup/cgroup_uninited.sql
+++ src/test/isolation2/sql/resgroup/cgroup_uninited.sql
@@ -4,8 +4,10 @@
 ! gpconfig -r gp_resource_manager;
 ! gpstop -rai;
 ! rmdir /sys/fs/cgroup/cpu/gpdb/*/;
--- */
+! rmdir /sys/fs/cgroup/cpuacct/gpdb/*/;
+-- to make vim happy */
 ! rmdir /sys/fs/cgroup/cpu/gpdb;
+! rmdir /sys/fs/cgroup/cpuacct/gpdb;
 -- end_ignore
 
 -- gpdb cgroup is missing
@@ -13,7 +15,9 @@
 
 -- start_ignore
 ! mkdir /sys/fs/cgroup/cpu/gpdb;
+! mkdir /sys/fs/cgroup/cpuacct/gpdb;
 ! chmod 644 /sys/fs/cgroup/cpu/gpdb;
+! chmod 644 /sys/fs/cgroup/cpuacct/gpdb;
 -- end_ignore
 
 -- no enough permission on gpdb cgroup
@@ -21,6 +25,7 @@
 
 -- start_ignore
 ! chmod 755 /sys/fs/cgroup/cpu/gpdb;
+! chmod 755 /sys/fs/cgroup/cpuacct/gpdb;
 ! chmod 444 /sys/fs/cgroup/cpu/gpdb/cgroup.procs;
 -- end_ignore
 
@@ -29,6 +34,7 @@
 
 -- start_ignore
 ! rmdir /sys/fs/cgroup/cpu/gpdb;
+! rmdir /sys/fs/cgroup/cpuacct/gpdb;
 ! gpconfig -r gp_resource_manager;
 -- end_ignore
 
--- src/test/isolation2/sql/resgroup/init_cgroup.sql
+++ src/test/isolation2/sql/resgroup/init_cgroup.sql
@@ -1,4 +1,7 @@
 -- start_ignore
 ! mkdir /sys/fs/cgroup/cpu/gpdb;
+! mkdir /sys/fs/cgroup/cpuacct/gpdb;
+
 ! chmod 755 /sys/fs/cgroup/cpu/gpdb;
+! chmod 755 /sys/fs/cgroup/cpuacct/gpdb;
 -- end_ignore
