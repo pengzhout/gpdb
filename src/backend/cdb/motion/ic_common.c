cloudberrydb/cloudberrydb

commit 353a937da9e9a1457a7bbbefb104f44d0f643415

restore TCP interconnect
This commit restore TCP interconnect and fix some hang issues.

* restore TCP interconnect code
* Add GUC called gp_interconnect_tcp_listener_backlog for tcp to control the backlog param of listen call
* use memmove instead of memcpy because the memory areas do overlap.
* call checkForCancelFromQD() for TCP interconnect if there are no data for a while, this can avoid QD from getting stuck.
* revert cancelUnfinished related modification in 8d251945d0, otherwise some queries will get stuck
* move and rename faultinjector "cursor_qe_reader_after_snapshot" to make test cases pass under TCP interconnect.

src/backend/cdb/motion/ic_common.c
@@ -24,6 +24,7 @@
 #include "cdb/tupchunklist.h"
 #include "cdb/ml_ipc.h"
 #include "cdb/cdbvars.h"
+#include "cdb/cdbdisp.h"
 
 #include <fcntl.h>
 #include <limits.h>
@@ -59,19 +60,9 @@
  * GLOBAL STATE VARIABLES
  */
 
-/* Socket file descriptor for the listener and sender. */
-int		ICListenerSocket;
-int 	ICSenderSocket;
-
-/*
- * Ports of Interconnect, assigned by initMotionLayerIPC() at process startup.
- * These ports are used for the duration of this process and should never change.
- */
-uint16	ICListenerPort;
-uint16 	ICSenderPort;
-
-int 	ICSenderFamily;
-
+/* Socket file descriptor for the listener. */
+int		TCP_listenerFd;
+int		UDP_listenerFd;
 
 /* Socket file descriptor for the sequence server. */
 int		savedSeqServerFd = -1;
@@ -107,7 +98,7 @@ logChunkParseDetails(MotionConn *conn)
 }
 
 TupleChunkListItem
-RecvTupleChunk(MotionConn *conn, bool inTeardown)
+RecvTupleChunk(MotionConn *conn, ChunkTransportState *transportStates)
 {
 	TupleChunkListItem tcItem;
 	TupleChunkListItem firstTcItem = NULL;
@@ -115,8 +106,19 @@ RecvTupleChunk(MotionConn *conn, bool inTeardown)
 	uint32		tcSize;
 	int			bytesProcessed = 0;
 
-	/* go through and form us some TupleChunks. */
-	bytesProcessed = sizeof(struct icpkthdr);
+	if (Gp_interconnect_type == INTERCONNECT_TYPE_TCP)
+	{
+		/* read the packet in from the network. */
+		readPacket(conn, transportStates);
+
+		/* go through and form us some TupleChunks. */
+		bytesProcessed = PACKET_HEADER_SIZE;
+	}
+	else
+	{
+		/* go through and form us some TupleChunks. */
+		bytesProcessed = sizeof(struct icpkthdr);
+	}
 
 #ifdef AMS_VERBOSE_LOGGING
 	elog(DEBUG5, "recvtuple chunk recv bytes %d msgsize %d conn->pBuff %p conn->msgPos: %p",
@@ -142,7 +144,7 @@ RecvTupleChunk(MotionConn *conn, bool inTeardown)
 		{
 			/* see MPP-720: it is possible that our message got messed
 			 * up by a cancellation ? */
-			ML_CHECK_FOR_INTERRUPTS(inTeardown);
+			ML_CHECK_FOR_INTERRUPTS(transportStates->teardownActive);
 
 			/*
 			 * MPP-4010: add some extra debugging.
@@ -162,6 +164,22 @@ RecvTupleChunk(MotionConn *conn, bool inTeardown)
 		}
 
 
+		/* we only check for interrupts here when we don't have a guaranteed full-message */
+		if (Gp_interconnect_type == INTERCONNECT_TYPE_TCP)
+		{
+			if (tcSize >= conn->msgSize)
+			{
+				/* see MPP-720: it is possible that our message got
+				 * messed up by a cancellation ? */
+				ML_CHECK_FOR_INTERRUPTS(transportStates->teardownActive);
+
+				logChunkParseDetails(conn);
+
+				ereport(ERROR, (errcode(ERRCODE_GP_INTERCONNECTION_ERROR),
+								errmsg("Interconnect error parsing message"),
+								errdetail("tcSize %d >= conn->msgSize %d", tcSize, conn->msgSize)));
+			}
+		}
 		Assert(tcSize < conn->msgSize);
 
 		/*
@@ -209,13 +227,20 @@ RecvTupleChunk(MotionConn *conn, bool inTeardown)
 void
 InitMotionLayerIPC(void)
 {
+	uint16 tcp_listener = 0;
+	uint16 udp_listener = 0;
+
 	/*activated = false;*/
 	savedSeqServerFd = -1;
 
-	/* use a local uint16 to remove warning of 'incompatible type' */
-	InitMotionUDPIFC();
+	if (Gp_interconnect_type == INTERCONNECT_TYPE_TCP)
+		InitMotionTCP(&TCP_listenerFd, &tcp_listener);
+	else if (Gp_interconnect_type == INTERCONNECT_TYPE_UDPIFC)
+		InitMotionUDPIFC(&UDP_listenerFd, &udp_listener);
 
-	elog(DEBUG1, "Interconnect listener port %d, sender port %d", ICListenerPort, ICSenderPort);
+	Gp_listener_port = (udp_listener<<16) | tcp_listener;
+
+	elog(LOG, "Interconnect listening on tcp port %d udp port %d (0x%x)", tcp_listener, udp_listener, Gp_listener_port);
 }
 
 /* See ml_ipc.h */
@@ -225,16 +250,22 @@ CleanUpMotionLayerIPC(void)
 	if (gp_log_interconnect >= GPVARS_VERBOSITY_DEBUG)
     	elog(DEBUG3, "Cleaning Up Motion Layer IPC...");
 
-	CleanupMotionUDPIFC();
+	if (Gp_interconnect_type == INTERCONNECT_TYPE_TCP)
+		CleanupMotionTCP();
+	else if (Gp_interconnect_type == INTERCONNECT_TYPE_UDPIFC)
+		CleanupMotionUDPIFC();
 
 	/* close down the Interconnect listener socket. */
+    if (TCP_listenerFd >= 0)
+        closesocket(TCP_listenerFd);
 
-	if (ICListenerSocket >= 0)
-		closesocket(ICListenerSocket);
+	if (UDP_listenerFd >= 0)
+		closesocket(UDP_listenerFd);
 
 	/* be safe and reset global state variables. */
-	ICListenerPort = 0;
-	ICListenerSocket = -1;
+	Gp_listener_port = 0;
+	TCP_listenerFd = -1;
+	UDP_listenerFd = -1;
 }
 
 /* See ml_ipc.h */
@@ -451,8 +482,27 @@ DeregisterReadInterest(ChunkTransportState *transportStates,
              reason);
     }
 
-	markUDPConnInactiveIFC(conn);
+	if (Gp_interconnect_type == INTERCONNECT_TYPE_UDPIFC)
+	{
+#ifdef AMS_VERBOSE_LOGGING
+		elog(LOG, "deregisterReadInterest set stillactive = false for node %d route %d (%s)", motNodeID, srcRoute, reason);
+#endif
+		markUDPConnInactiveIFC(conn);
+	}
+	else
+	{
+		/*
+		 * we also mark the connection as "done." The way synchronization works is
+		 * strange. On QDs the "teardown" doesn't get called until all segments
+		 * are finished, which means that we need some way for the QEs to know
+		 * that Teardown should complete, otherwise we deadlock the entire query
+		 * (QEs wait in their Teardown calls, while the QD waits for them to
+		 * finish)
+		 */
+		shutdown(conn->sockfd, SHUT_WR);
 
+		MPP_FD_CLR(conn->sockfd, &pEntry->readSet);
+	}
 	return;
 }
 
@@ -604,10 +654,32 @@ setupSeqServerConnection(char *seqServerHost, uint16 seqServerPort)
 void
 SetupInterconnect(EState *estate)
 {
-	SetupUDPIFCInterconnect(estate);
-	return;
+	if (Gp_interconnect_type == INTERCONNECT_TYPE_UDPIFC)
+		SetupUDPIFCInterconnect(estate);
+	else if (Gp_interconnect_type == INTERCONNECT_TYPE_TCP)
+		SetupTCPInterconnect(estate);
 }
 
+/*
+ * Move this out to separate stack frame, so that we don't have to mark
+ * tons of stuff volatile in TeardownInterconnect().
+ */
+void
+forceEosToPeers(MotionLayerState       *mlStates,
+				ChunkTransportState    *transportStates,
+				int                     motNodeID)
+{
+	if (!transportStates)
+	{
+		elog(FATAL, "no transport-states.");
+	}
+
+	transportStates->teardownActive = true;
+
+	transportStates->SendEos(mlStates, transportStates, motNodeID, get_eos_tuplechunklist());
+
+	transportStates->teardownActive = false;
+}
 
 /* TeardownInterconnect() function is used to cleanup interconnect resources that
  * were allocated during SetupInterconnect().  This function should ALWAYS be
@@ -619,8 +691,14 @@ TeardownInterconnect(ChunkTransportState *transportStates,
 					 MotionLayerState *mlStates,
 					 bool forceEOS)
 {
-	TeardownUDPIFCInterconnect(transportStates, mlStates, forceEOS);
-	return;
+	if (Gp_interconnect_type == INTERCONNECT_TYPE_UDPIFC)
+	{
+		TeardownUDPIFCInterconnect(transportStates, mlStates, forceEOS);
+	}
+	else if (Gp_interconnect_type == INTERCONNECT_TYPE_TCP)
+	{
+		TeardownTCPInterconnect(transportStates, mlStates, forceEOS);
+	}
 }
 
 /*=========================================================================
@@ -705,6 +783,7 @@ createChunkTransportState(ChunkTransportState *transportStates,
         conn->tupleCount = 0;
         conn->stillActive = false;
         conn->stopRequested = false;
+        conn->wakeup_ms = 0;
         conn->cdbProc = NULL;
         conn->sent_record_typmod = 0;
         conn->remapper = NULL;
@@ -749,6 +828,8 @@ removeChunkTransportState(ChunkTransportState *transportStates,
 		transportStates->states[motNodeID - 1].valid = false;
 		pEntry = &transportStates->states[motNodeID - 1];
 	}
+
+	MPP_FD_ZERO(&pEntry->readSet);
 
 	return pEntry;
 }
@@ -805,6 +886,27 @@ void adjustMasterRouting(Slice *recvSlice)
 }
 
 /*
+ * checkForCancelFromQD
+ * 		Check for cancel from QD.
+ *
+ * Should be called only inside the dispatcher
+ */
+void
+checkForCancelFromQD(ChunkTransportState *pTransportStates)
+{
+	Assert(Gp_role == GP_ROLE_DISPATCH);
+	Assert(pTransportStates);
+	Assert(pTransportStates->estate);
+
+	if (cdbdisp_checkForCancel(pTransportStates->estate->dispatcherState))
+	{
+		ereport(ERROR, (errcode(ERRCODE_GP_INTERCONNECTION_ERROR),
+						errmsg(CDB_MOTION_LOST_CONTACT_STRING)));
+		/* not reached */
+	}
+}
+
+/*
 * WaitInterconnectQuit
 *
 * Wait for the ic thread to quit, don't clean any resource owned by ic thread
@@ -812,5 +914,8 @@ void adjustMasterRouting(Slice *recvSlice)
 void
 WaitInterconnectQuit(void)
 {
-	WaitInterconnectQuitUDPIFC();
+	if (Gp_interconnect_type == INTERCONNECT_TYPE_UDPIFC)
+	{
+		WaitInterconnectQuitUDPIFC();
+	}
 }

gpMgmt/bin/gppylib/programs/clsInjectFault.py
@@ -422,7 +422,7 @@ class GpInjectFaultProgram:
                   "process_startup_packet (inject fault when processing startup packet during backend initialization), " \
                   "quickdie (inject fault when auxiliary processes quitting), " \
                   "after_one_slice_dispatched (inject fault after one slice was dispatched when dispatching plan), " \
-                  "cursor_qe_reader_after_snapshot (inject fault after QE READER has populated snashot for cursor)" \
+                  "qe_got_snapshot_and_interconnect(inject fault after qe got snapshot and interconnect)" \
 			      "fsync_counter (inject fault to count buffers fsync'ed by checkpoint process), " \
 			      "bg_buffer_sync_default_logic (inject fault to 'skip' in order to flush all buffers in BgBufferSync()), " \
                   "finish_prepared_after_record_commit_prepared (inject fault in FinishPreparedTransaction() after recording the commit prepared record), " \

src/backend/cdb/cdbvars.c
@@ -179,6 +179,13 @@ bool		gp_enable_slow_writer_testmode = false;
  */
 bool		gp_enable_slow_cursor_testmode = false;
 
+/*
+ * TCP port the Interconnect listens on for incoming connections from other
+ * backends.  Assigned by initMotionLayerIPC() at process startup.  This port
+ * is used for the duration of this process and should never change.
+ */
+int			Gp_listener_port;
+
 int			Gp_max_packet_size; /* max Interconnect packet size */
 
 int			Gp_interconnect_queue_depth = 4;	/* max number of messages
@@ -200,6 +207,8 @@ int			Gp_interconnect_hash_multiplier = 2;		/* sets the size of the
 int			interconnect_setup_timeout = 7200;
 
 int			Gp_interconnect_type = INTERCONNECT_TYPE_UDPIFC;
+
+bool		gp_interconnect_aggressive_retry = true; /* fast-track app-level retry */
 
 bool		gp_interconnect_full_crc = false;	/* sanity check UDP data. */
 
@@ -886,12 +895,13 @@ gpvars_assign_gp_interconnect_type(const char *newval, bool doit, GucSource sour
 {
 	int			newtype = 0;
 
-	if (newval == NULL || newval[0] == 0)
+	if (newval == NULL || newval[0] == 0 ||
+		!pg_strcasecmp("udpifc", newval))
 		newtype = INTERCONNECT_TYPE_UDPIFC;
-	else if (!pg_strcasecmp("udpifc", newval))
-		newtype = INTERCONNECT_TYPE_UDPIFC;
+	else if (!pg_strcasecmp("tcp", newval))
+		newtype = INTERCONNECT_TYPE_TCP;
 	else
-		elog(ERROR, "Only support UDPIFC, (current type is '%s')", gpvars_show_gp_interconnect_type());
+		elog(ERROR, "Unknown interconnect type. (current type is '%s')", gpvars_show_gp_interconnect_type());
 
 	if (doit)
 	{
@@ -904,8 +914,15 @@ gpvars_assign_gp_interconnect_type(const char *newval, bool doit, GucSource sour
 const char *
 gpvars_show_gp_interconnect_type(void)
 {
-	return "UDPIFC";
-}	/* gpvars_show_gp_log_interconnect */
+	switch(Gp_interconnect_type)
+	{
+		case INTERCONNECT_TYPE_TCP:
+			return "TCP";
+		case INTERCONNECT_TYPE_UDPIFC:
+		default:
+			return "UDPIFC";
+	}
+}                               /* gpvars_show_gp_log_interconnect */
 
 /*
  * gpvars_assign_gp_interconnect_fc_method

src/backend/cdb/dispatcher/cdbconn.c
@@ -395,8 +395,11 @@ void cdbconn_doConnect(SegmentDatabaseDescriptor *segdbDesc,
 		else
 		{
 			if (gp_log_gang >= GPVARS_VERBOSITY_DEBUG)
-				write_log("Connected to %s motionListener=%d with options: %s\n",
-						segdbDesc->whoami, segdbDesc->motionListener, options);
+				write_log("Connected to %s motionListener=%d/%d with options: %s\n",
+						  segdbDesc->whoami,
+						  (segdbDesc->motionListener & 0x0ffff),
+						  ((segdbDesc->motionListener>>16) & 0x0ffff),
+						  options);
 		}
 	}
 }

src/backend/cdb/dispatcher/cdbgang.c
@@ -37,7 +37,6 @@
 #include "cdb/cdbtm.h"			/* discardDtxTransaction() */
 #include "cdb/cdbutil.h"		/* CdbComponentDatabaseInfo */
 #include "cdb/cdbvars.h"		/* Gp_role, etc. */
-#include "cdb/ml_ipc.h"
 #include "storage/bfz.h"
 #include "gp-libpq-fe.h"
 #include "gp-libpq-int.h"
@@ -950,7 +949,12 @@ static CdbProcess *makeCdbProcess(SegmentDatabaseDescriptor *segdbDesc)
 	}
 
 	process->listenerAddr = pstrdup(qeinfo->hostip);
-	process->listenerPort = segdbDesc->motionListener;
+
+	if (Gp_interconnect_type == INTERCONNECT_TYPE_UDPIFC)
+		process->listenerPort = (segdbDesc->motionListener >> 16) & 0x0ffff;
+	else if (Gp_interconnect_type == INTERCONNECT_TYPE_TCP)
+		process->listenerPort = (segdbDesc->motionListener & 0x0ffff);
+
 	process->pid = segdbDesc->backendPid;
 	process->contentid = segdbDesc->segindex;
 	return process;
@@ -1049,7 +1053,11 @@ getCdbProcessesForQD(int isPrimary)
 	 * interconnect connection.
 	 */
 	proc->listenerAddr = NULL;
-	proc->listenerPort = ICListenerPort;
+
+	if (Gp_interconnect_type == INTERCONNECT_TYPE_UDPIFC)
+		proc->listenerPort = (Gp_listener_port >> 16) & 0x0ffff;
+	else if (Gp_interconnect_type == INTERCONNECT_TYPE_TCP)
+		proc->listenerPort = (Gp_listener_port & 0x0ffff);
 
 	proc->pid = MyProcPid;
 	proc->contentid = -1;

src/backend/cdb/motion/Makefile
@@ -13,6 +13,6 @@ include $(top_builddir)/src/Makefile.global
 override CPPFLAGS := -I$(top_srcdir)/src/backend/gp_libpq_fe $(CPPFLAGS)
 
 OBJS = cdbmotion.o tupchunklist.o tupser.o  \
-	ic_common.o ic_udpifc.o htupfifo.o tupleremap.o
+	ic_common.o ic_tcp.o ic_udpifc.o htupfifo.o tupleremap.o
 
 include $(top_srcdir)/src/backend/common.mk

src/backend/cdb/motion/cdbmotion.c
@@ -162,7 +162,10 @@ initMotionLayerStructs(MotionLayerState **mlStates)
 	if (Gp_role == GP_ROLE_UTILITY)
 		return;
 
-	Gp_max_tuple_chunk_size = Gp_max_packet_size - sizeof(struct icpkthdr) - TUPLE_CHUNK_HEADER_SIZE;
+	if (Gp_interconnect_type == INTERCONNECT_TYPE_UDPIFC)
+		Gp_max_tuple_chunk_size = Gp_max_packet_size - sizeof(struct icpkthdr) - TUPLE_CHUNK_HEADER_SIZE;
+	else if (Gp_interconnect_type == INTERCONNECT_TYPE_TCP)
+		Gp_max_tuple_chunk_size = Gp_max_packet_size - PACKET_HEADER_SIZE - TUPLE_CHUNK_HEADER_SIZE;		
 
 	/*
 	 * Use the statically allocated chunk that is intended for sending end-of-
@@ -567,6 +570,12 @@ SendTuple(MotionLayerState *mlStates,
 	return rc;
 }
 
+TupleChunkListItem
+get_eos_tuplechunklist(void)
+{
+	return s_eos_chunk_data;
+}
+
 /*
  * Sends a token to all peer Motion Nodes, indicating that this motion
  * node has no more tuples to send out.
@@ -776,7 +785,8 @@ processIncomingChunks(MotionLayerState *mlStates,
 	}
 
 	/* The chunk list we just processed freed-up our rx-buffer space. */
-	MlPutRxBufferIFC(transportStates, motNodeID, srcRoute);
+	if (Gp_interconnect_type == INTERCONNECT_TYPE_UDPIFC)
+		MlPutRxBufferIFC(transportStates, motNodeID, srcRoute);
 
 	/* Stats */
 	statChunksProcessed(mlStates, pMNEntry, numChunks, chunkBytes, tupleBytes);

pengzhout/gpdb

commit 40baf0bba8e683edbc71d389bc011a166388cdb6

Log interconnect ports info on DEBUG1 level
This is a mistake introduced by 353a937da9e9 and it
flood the pg_log at a noticeable rate, change it back
to DEBUG1.

src/backend/cdb/motion/ic_common.c
@@ -240,7 +240,7 @@ InitMotionLayerIPC(void)
 
 	Gp_listener_port = (udp_listener<<16) | tcp_listener;
 
-	elog(LOG, "Interconnect listening on tcp port %d udp port %d (0x%x)", tcp_listener, udp_listener, Gp_listener_port);
+	elog(DEBUG1, "Interconnect listening on tcp port %d udp port %d (0x%x)", tcp_listener, udp_listener, Gp_listener_port);
 }
 
 /* See ml_ipc.h */