45d91cf6244481dac97fa9f35996ff485fedc40a
Pengzhou Tang <ptang@pivotal.io>
2018-07-14 00:22:52 -0400

    change type of db_descriptors to SegmentDatabaseDescriptor **
    A new gang definination may consist of cached segdbDesc and new
    created segdbDesc, there is no need to palloc all segdbDesc struct
    as new.

--- src/backend/cdb/dispatcher/cdbdisp_async.c
+++ src/backend/cdb/dispatcher/cdbdisp_async.c
@@ -291,7 +291,7 @@ cdbdisp_dispatchToGang_async(struct CdbDispatcherState *ds,
 	{
 		CdbDispatchResult *qeResult;
 
-		SegmentDatabaseDescriptor *segdbDesc = &gp->db_descriptors[i];
+		SegmentDatabaseDescriptor *segdbDesc = gp->db_descriptors[i];
 
 		Assert(segdbDesc != NULL);
 
--- src/include/cdb/cdbgang.h
+++ src/include/cdb/cdbgang.h
@@ -52,7 +52,7 @@ typedef struct Gang
 	/*
 	 * Array of QEs/segDBs that make up this gang. Sorted by segment index.
 	 */
-	struct SegmentDatabaseDescriptor *db_descriptors;	
+	struct SegmentDatabaseDescriptor **db_descriptors;	
 
 	/* For debugging purposes only. These do not add any actual functionality. */
 	bool allocated;
