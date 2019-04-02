41b891b86d123b060a3b8b400cb07c3d7c0670b2
Pengzhou Tang <ptang@pivotal.io>
2019-04-02 01:42:57 -0400

    Always wait until the distributed transactions are recovered
    In commit 48cb340d41, we spawn a dedicated process to recovery distributed
    transaction, newly requested connections will error out if dtx can't be
    recovered in 4 seconds. This sounds correct to error out quickly because the
    system is indeed unavailable, however, for some utilities like gpstart and
    gprecoverseg, an error like this will be treated as a very scary issue and
    will fail the utilities.
    
    4 seconds is designed to notify the users as soon as possible, now it seems
    better to keep the old behave before 48cb340d41 which is always waiting until
    the dtx is recovered.

--- src/backend/cdb/cdbutil.c
+++ src/backend/cdb/cdbutil.c
@@ -892,9 +892,6 @@ cdbcomponent_getComponentInfo(int contentId)
 void
 cdb_setup(void)
 {
-	int	i;
-	static const int DtxRecoveryWaitTime = 40;
-
 	elog(DEBUG1, "Initializing Greenplum components...");
 
 	/* If gp_role is UTILITY, skip this call. */
@@ -910,16 +907,12 @@ cdb_setup(void)
 	 */
 	if (Gp_role == GP_ROLE_DISPATCH && !*shmDtmStarted)
 	{
-		for (i = 0; i < DtxRecoveryWaitTime; i++)
+		while (true)
 		{
 			pg_usleep(100 * 1000); /* 100ms */
 			if (*shmDtmStarted)
-				return;
+				break;
 		}
-
-		ereport(FATAL,
-				(errcode(ERRCODE_CANNOT_CONNECT_NOW),
-				 errmsg("the database system is recovering distributed transactions")));
 	}
 }
 
